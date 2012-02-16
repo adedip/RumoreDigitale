7c75c5ff387898ef0e4651f8808aa6c518464a22
Andrea D'Ippolito <adedip@gmail.com>
2012-02-16 19:10:13 +0100

    fix inner link

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -29,7 +29,7 @@ class FeedEntry < ActiveRecord::Base
           unless exists? :guid => entry.id
             create!(
               :name         => entry.title.sub(/ il /,": "),
-              :summary      => entry.summary.gsub(/"\/wp-content/, '"http://www.rumoredigitale.com/wp-content'),
+              :summary      => entry.summary.gsub(/"\/wp-content/, '"http://www.rumoredigitale.com/wp-content').gsub(/href="\//, 'href="http://www.rumoredigitale.com/'),
               :url          => entry.url,
               :published_at => entry.published - 1.hours,
               :guid         => entry.id,
